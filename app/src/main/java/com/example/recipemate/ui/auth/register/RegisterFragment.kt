820fb949a86b76ad9b24262b96578b35e0c7e986
Mohamed Esam <moesam138@gmail.com>
2024-08-17 17:12:17 +0300

    Implement RegisterFragment layout

--- app/src/main/java/com/example/recipemate/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/recipemate/ui/auth/register/RegisterFragment.kt
@@ -1,8 +1,28 @@
 package com.example.recipemate.ui.auth.register
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipemate.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
