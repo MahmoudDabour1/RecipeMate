a45578ed72c81706dc0e9457b9b4cbbca835c8c8
MohamedAttia3104 <attia3104@gmail.com>
2024-08-18 15:45:09 +0300

    update recipe details ui

--- app/src/main/java/com/example/recipemate/ui/recipe/recipeDetails/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/recipemate/ui/recipe/recipeDetails/RecipeDetailsFragment.kt
@@ -1,7 +1,26 @@
 package com.example.recipemate.ui.recipe.recipeDetails
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipemate.databinding.FragmentRecipeDetailsBinding
 
 class RecipeDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentRecipeDetailsBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRecipeDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
