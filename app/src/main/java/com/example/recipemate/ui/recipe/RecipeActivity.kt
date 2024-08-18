d28d00379e53887776e09af3de1f908e42018d10
MohamedAttia3104 <attia3104@gmail.com>
2024-08-18 19:55:31 +0300

    remove bottom nav bar when navigate to recipe details

--- app/src/main/java/com/example/recipemate/ui/recipe/RecipeActivity.kt
+++ app/src/main/java/com/example/recipemate/ui/recipe/RecipeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.recipemate.ui.recipe
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.recipemate.R
@@ -10,6 +12,8 @@ import com.example.recipemate.ui.recipe.home.RecipeViewModel
 
 class RecipeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeBinding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,15 @@ class RecipeActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.recipeDetailsFragment -> binding.bottomNavigationView.visibility = View.GONE
+                else -> binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
 
     }
 }
